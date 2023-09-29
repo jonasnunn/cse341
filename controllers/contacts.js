412735716f725b35444e6f61b740448c5cec79c0
jonasnunn <jonasnunn@gmail.com>
2023-09-29 10:04:10 -0600

    small fixes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -79,7 +79,7 @@ const deleteContact = async (req, res) => {
     .deleteOne({ _id: contact }, true);
   console.log(result);
   if (result.deletedCount > 0) {
-    res.status(204).send();
+    res.status(200).send();
   } else {
     res
       .status(500)
