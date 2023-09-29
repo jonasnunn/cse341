db420f655274efe1ac0691d1cb41bbf91cc5ed4d
jonasnunn <jonasnunn@gmail.com>
2023-09-29 09:54:08 -0600

    finished week 3 assignment

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,10 +1,10 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
+  const result = await mongodb.getDb().db().collection("contacts").find();
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists);
   });
 };
@@ -14,12 +14,83 @@ const getSingle = async (req, res, next) => {
   const result = await mongodb
     .getDb()
     .db()
-    .collection('contacts')
+    .collection("contacts")
     .find({ _id: userId });
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists[0]);
   });
 };
 
-module.exports = { getAll, getSingle };
+// new code
+const postContact = async (req, res) => {
+  const newContact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection("contacts")
+    .insertOne(newContact);
+  if (result.acknowledged) {
+    res.status(201).json(result);
+  } else {
+    res
+      .status(500)
+      .json(result.error || "ERROR: The contact could not be created.");
+  }
+};
+
+const updateContact = async (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection("contacts")
+    .replaceOne({ _id: contactId }, contact);
+  if (result.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(
+        result.error || "ERROR: The contact could not be updated."
+      );
+  }
+};
+
+const deleteContact = async (req, res) => {
+  const contact = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection("contacts")
+    .deleteOne({ _id: contact }, true);
+  console.log(result);
+  if (result.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(result.error || "ERROR: The contact was not able to be deleted");
+  }
+};
+
+module.exports = {
+  getAll,
+  getSingle,
+  postContact,
+  updateContact,
+  deleteContact,
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,8 +3,15 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
+
+
 router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
+
+// new code
+router.post('/', contactsController.postContact);
+router.put('/:id', contactsController.updateContact);
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
