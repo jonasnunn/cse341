0c2e983f5c4bf9eb3c1abd6682053c0e2cc40876
jonasnunn <jonasnunn@gmail.com>
2023-10-07 22:30:41 -0600

    route change

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'Contacts API Documentation',
     description: 'My contacts',
   },
-  host: 'localhost:8080',
+  host: 'https://cse341-senp.onrender.com/',
   schemes: ['http'],
 };
 
