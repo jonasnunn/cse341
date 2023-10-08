5e49b57a823f484480ccbefa2efbfcfcf2a42faf
jonasnunn <jonasnunn@gmail.com>
2023-10-07 23:16:01 -0600

    test

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'My contacts',
   },
   host: 'cse341-senp.onrender.com',
-  schemes: ['http'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger-output.json';
