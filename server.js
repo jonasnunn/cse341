af39815720db5dd43d480471c5fb21027eebbc84
jonasnunn <jonasnunn@gmail.com>
2023-10-07 19:03:51 -0600

    week 4 stuff

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./db/connect");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-output.json");
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -8,10 +10,11 @@ const app = express();
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     next();
   })
-  .use('/', require('./routes'));
+  .use("/", require("./routes"))
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
---
+++ swagger.js
@@ -0,0 +1,21 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Contacts API Documentation',
+    description: 'My contacts',
+  },
+  host: 'localhost:8080',
+  schemes: ['http'],
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./server.js'];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+    require('./server.js'); // Your project's root file
+  });
+
